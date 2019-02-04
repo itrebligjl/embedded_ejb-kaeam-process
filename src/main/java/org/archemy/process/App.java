0eb1b1bb1275df200f7f2921012542808907c08b
Joanna Gilberti <jgilberti@archemy.com>
2019-02-03 19:56:03 -0500

    add code

---
+++ src/main/java/org/archemy/process/App.java
@@ -0,0 +1,13 @@
+package org.archemy.process;
+
+/**
+ * Archemy EAMTk KAEAM
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Archemy EAMTk KAEAM Process!" );
+    }
+}
